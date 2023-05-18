7a700efbac1bb7224bcad8bee740c2572944ff01
saijasha <133983652+saijasha@users.noreply.github.com>
2023-05-18 11:42:51 -0400

    Update Java2.java
    for loop added

--- src/java2/Java2.java
+++ src/java2/Java2.java
@@ -15,6 +15,8 @@ public class Java2 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for (int i = 0; i < 10; i++)
+            System.out.print(i + " ");
     }
     
 }
